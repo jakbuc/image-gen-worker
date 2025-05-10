1e8ca83f195b873eff1aa93b16077f35a603eb5f
jakbuc <33811375+jakbuc@users.noreply.github.com>
2025-05-10 21:27:14 +0200

    Add query param

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export default {
   async fetch(request, env) {
-    const inputs = {
-      prompt: "cyberpunk cat",
-    };
+    const url = new URL(request.url);
+    const prompt = url.searchParams.get("prompt") || "cyberpunk cat";
+    const inputs = { prompt };
 
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
